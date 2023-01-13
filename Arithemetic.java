70b0312b52e71dfe260bf86e7bfce1796dca96bf
brcmkd <121794872+brcmkd@users.noreply.github.com>
2023-01-13 19:51:34 +0530

    COMMITTING JAVA PROGRAMMES

---
+++ Arithemetic.java
@@ -0,0 +1,14 @@
+class Arithemetic
+{
+public static void main(String [] args)
+{
+int a=6,b=10;
+sum(a,b);
+}
+public static void sum(int a,int b)
+{
+int sum=a+b;
+System.out.println("sum="+sum);
+//return sum;
+}
+}
